0e6b86aea40f3e5a4e9bf149b4c0baa8d6956ebb
Oswaldo Linhares <oswaldolinhares@gmail.com>
2024-01-21 00:02:35 -0300

    feat(Rspec): Add import support folder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,10 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join('spec/support/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
